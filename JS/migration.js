806a5720be6ed5fe95cde6681433d72ba37730af
nabilouhi <nabilouhi@hotmail.com>
2019-01-04 15:09:11 +0100

    Update migration.js

--- JS/migration.js
+++ JS/migration.js
@@ -1,20 +1,20 @@
 ﻿function migration(){
 
-var info = '<div class="container"><h3>Territoire</h3> '+
+var info = '<div class="container"><h3>Territoire [En construction ...]</h3> '+
 
 		'<ul class="nav nav-pills">'+
 			'<li class="active"><a data-toggle="pill" href="#menu1">Migration Pendulaire</a></li>'+
-			'<li><a data-toggle="pill" href="#menu2">Menu2</a></li>'+
+			'<li><a data-toggle="pill" href="#menu2">Carte 2</a></li>'+
 		'</ul>' +
 		
 		 '<div class="tab-content">'+
 		 
 			'<div id="menu1" class="tab-pane fade in active">'+
-				'<p>Station meteologique</p>'+
+				'<p>[En construction ...]</p>'+
 				'</div>'+
 				
 			'<div id="menu2" class="tab-pane fade ">'+
-				'<p>Station meteologique2222</p>'+
+				'<p>[En construction ...]</p>'+
 				'</div>'+
 		' </div>';
 document.getElementById("INFO").innerHTML=info;
