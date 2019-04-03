0e9238f16b259f96d35d33f1851c7eb9a7f23d9f
nabilouhi <nabilouhi@hotmail.com>
2019-04-03 11:00:48 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 ﻿let express = require('express')
 let PNG = require('save-svg-as-png')
 let app =express()
+const fs = require('fs');
 var router = express.Router();
 
 const port=process.env.PORT || 8080
@@ -9,29 +10,35 @@ const port=process.env.PORT || 8080
 app.set('view engine','ejs')
 app.use(express.static('public'))
 //app.use(PNG)
-
+infodata=[];
+fs.readdir("radartxt", function(err, items) {
+	for (var i=0; i<items.length; i++) {
+		infodata[i]=fs.readFileSync('radartxt/'+items[i],'utf8');
+		console.log(items[i]);
+    }	
+});
 
 
 app.get('/Environnement',(request,response)=>{
 	response.render('Environnement', {
 		data:[
-			{name:"ex1",titre:"Pas de Pauvereté"},
-			{name:"ex2",titre:"Faim ZERO"},
-			{name:"ex3",titre:"Bonne santé"},
-			{name:"ex4",titre:"Education de qualité"},
-			{name:"ex5",titre:"Egalité entre les sexes"},
-			{name:"ex6",titre:"Eau propre et assainissemnts"},
-			{name:"ex7",titre:"Energie propre et abordable"},
-			{name:"ex8",titre:"Travail descent et croissance"},
-			{name:"ex9",titre:"Industrie et inovation"},
-			{name:"ex10",titre:"Ineaglités reduites"},
-			{name:"ex11",titre:"Communautés durables"},
-			{name:"ex12",titre:"Consommation responsable"},
-			{name:"ex13",titre:"Lutte contre le chgmt climatique"},
-			{name:"ex14",titre:"Vie aquatique"},
-			{name:"ex15",titre:"Vie terrestre"},
-			{name:"ex16",titre:"Paix et justice efficase"},
-			{name:"ex17",titre:"Partenariats pour la real. des ODD"},
+			{name:"ex1",titre:"Pas de pauvreté",TEXT:infodata[0]},
+			{name:"ex2",titre:"Faim zéro",TEXT:infodata[1]},
+			{name:"ex3",titre:"Bonne santé",TEXT:infodata[2]},
+			{name:"ex4",titre:"Education de qualité",TEXT:infodata[3]},
+			{name:"ex5",titre:"Egalité entre les sexes",TEXT:infodata[4]},
+			{name:"ex6",titre:"Eau propre et assainissement",TEXT:infodata[5]},
+			{name:"ex7",titre:"Energie propre et abordable",TEXT:infodata[6]},
+			{name:"ex8",titre:"Travail décent et croissance",TEXT:infodata[7]},
+			{name:"ex9",titre:"Industrie et innovation",TEXT:infodata[8]},
+			{name:"ex10",titre:"Inégalités réduites",TEXT:infodata[9]},
+			{name:"ex11",titre:"Communautés durables",TEXT:infodata[10]},
+			{name:"ex12",titre:"Consommation responsable",TEXT:infodata[11]},
+			{name:"ex13",titre:"Lutte contre le chgt climatique",TEXT:infodata[12]},
+			{name:"ex14",titre:"Vie aquatique",TEXT:infodata[13]},
+			{name:"ex15",titre:"Vie terrestre",TEXT:infodata[14]},
+			{name:"ex16",titre:"Paix et justice efficace",TEXT:infodata[15]},
+			{name:"ex17",titre:"Partenariats pour les objectifs",TEXT:infodata[16]}
 		]}
 	)
 })
